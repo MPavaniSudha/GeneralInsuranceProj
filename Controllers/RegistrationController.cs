a6dcf57bbd7e0d3ae15bd916b05ac43a4226038e
MPavaniSudha <98110996+MPavaniSudha@users.noreply.github.com>
2022-03-30 16:10:06 +0530

    GeneralInsurance

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeneralInsurancePrj.Models;
+
+namespace GeneralInsurancePrj.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        InsuranceDBEntities idb = new InsuranceDBEntities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RegistrationDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrationDetails(RegistrationInfo reginfo)
+        {
+            idb.RegistrationInfoes.Add(reginfo);
+            idb.SaveChanges();
+            return View(reginfo);
+        }
+    }
+}
